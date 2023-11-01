8235c1950ed5b1332657fea287fc0a6c8f4cb6a9
Satvik1769 <manchandasatvik@gmail.com>
2023-11-02 04:22:31 +0530

    api

---
+++ src/app/api/db/createQuestion/route.js
@@ -0,0 +1,13 @@
+import prisma from "../../../../../prisma/prismaClient"
+
+async function POST(req){
+    let { ques, correct,type, examId  }= await req.json();
+    examId = BigInt(examId);
+    const question = await prisma.questions.create({
+        data:{ques,correct,type, examId:examId.toString()}
+        
+    })
+    console.log(question)
+    return new Response(JSON.stringify(question))
+}
+export { POST as POST };
--- src/app/api/db/getUsers/[user]/route.js
+++ src/app/api/db/getUsers/[user]/route.js
@@ -1,14 +1,17 @@
 import prisma from "../../../../../../prisma/prismaClient";
 
 async function handler(req, { params }) {
+  const { id, username, password} = await req.json();
+  console.log(id, username, password);
   const { user } = params;
   console.log("user", user);
   const u = await prisma.users.findUnique({
     where: {
-      username: user,
+      username,
     },
   });
-  return new Response(JSON.stringify(u, user));
+  
+  return new Response(JSON.stringify(username,password));
 }
 
-export { handler as GET, handler as POST };
+export { handler as GET };
