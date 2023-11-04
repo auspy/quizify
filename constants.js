a0cd28f9eecd40a200441c8aafbad964ffc293e6
Satvik1769 <manchandasatvik@gmail.com>
2023-11-04 18:23:22 +0530

    update

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const urlLocal =  "http://localhost:3000/";
+export const urlLocal = process.env.NEXT_PUBLIC_SITE_URL || "http//localhost:3000/" || "";
 console.log("--- PUBLIC URL ==> ", urlLocal);
 // export const urlLocal = "";
